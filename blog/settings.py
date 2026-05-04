161b04c9f845057f46bab3e28e19024f516bbfa8
ridayasth4 <ridaya350@gmail.com>
2026-05-05 00:02:54 +0545

    fixed

--- blog/settings.py
+++ blog/settings.py
@@ -103,6 +103,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
